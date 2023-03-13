7880a56b2d9ffaf7f0faadc18c7900949be71e06
722sofiaameh <sofiagabriel722@gmail.com>
2023-03-13 12:16:10 -0700

    add navbar component

---
+++ pages/components/navbar.tsx
@@ -0,0 +1,37 @@
+export const ProjectNavbar = () => {
+    return (
+        <>
+        <div className="flex items-center justify-between container mx-auto pt-6">
+          <div>
+              <h1 className="text-3xl font-sora text-darkGray">Sof's-folio</h1>
+          </div>
+          <ul className="flex gap-10 items-center font-mulish text-lg text-lightGray">
+            <li><a href="/">Projects</a></li>
+            <li><a href="/">About Me</a></li>
+            <li><a href="/">Contact</a></li>
+
+          </ul>
+        </div>
+        </>
+    );
+}
+ 
+
+export const  WorkNavbar= () => {
+    return (
+        <>
+        <div className="flex items-center justify-between container mx-auto pt-6">
+          <div>
+              <h1 className="text-3xl font-sora text-darkGray">Sof's-folio</h1>
+          </div>
+          <ul className="flex gap-10 items-center font-mulish text-lg text-lightGray">
+            <li><a href="/">Work</a></li>
+            <li><a href="/">About Me</a></li>
+            <li><a href="/">Contact</a></li>
+
+          </ul>
+        </div>
+        </>
+    );
+}
+ 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
   "./pages/**/*.{js,ts,jsx,tsx}",
   "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sora': ['Sora', 'sans-serif'],
+        'mulish': ['Mulish', 'sans-serif'],
+      },
+      colors : {
+        backroundWhite : '#FFFFFFFF',
+        darkGray : ' #171A1FFF',
+        lightGray : ' #323842FF',
+        lightBlue: '#3EBDE0FF',
+        lightGreen : '#83AA19FF',
+        linkColor: ' #424955FF',
+        footerColor: ' #1D2128FF',
+        blueBackgroundColor: '#F2FAFDFF',
+        grayBackgroundColor : '#F6FCE8FF'
+      }
+    },
   },
   plugins: [],
 }
