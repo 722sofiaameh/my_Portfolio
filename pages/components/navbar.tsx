ad5680c3865fd471a28e42a3486f6d7283bc0892
722sofiaameh <sofiagabriel722@gmail.com>
2023-04-24 12:00:10 +0100

    refactor code

--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -2,23 +2,28 @@ import Link from "next/link";
 
  const ProjectNavbar = () => {
   return (
-    <div className="bg-navPink">
-      <nav className="flex items-center justify-between container mx-auto pt-6 pb-6">
+    <div className="s-header bg-black">
+      <nav className="row  flex items-center justify-between container mx-auto pt-6 pb-6 bg-black">
         <div className="flex justify-center items-center gap-10">
-          <h1 className="text-3xl font-Pattaya text-lightGray ">Sophia Ameh </h1>
-          
+        <div className="s-header__block">
+                    <div className="s-header__logo">
+                        <a className="logo" href="index.html" style={{color
+                        :'white'}}>
+                        
+                            sOf-fOlIo
+                        </a>
+                    </div>
+
+                    <a className="s-header__menu-toggle" href="#0"><span>Menu</span></a>
+                </div>
           <div className="loader"></div>
         </div>
-        <ul className="flex gap-10 items-center font-mulish text-lg text-white">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/">Service</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
+        <ul className="s-header__menu-links flex gap-10 items-center font-mulish text-lg text-white">
+                        <li className="current"><a className="smoothscroll" href="#intro">Intro</a></li>
+                        <li><a className="smoothscroll" href="#about">About</a></li>
+                        <li><a className="smoothscroll" href="#works">Works</a></li>
+                        <li><a className="smoothscroll" href="#footer">Contact</a></li>
+                   
         </ul>
       </nav>
     </div>
